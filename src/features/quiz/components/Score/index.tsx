923cbbb84eab3fef1e14cd74686d1f6c127bf267
7uyf@protonmail.com <7uyf@protonmail.com>
2024-11-13 16:42:01 +0200

    score page

--- src/features/quiz/components/Score/index.tsx
+++ src/features/quiz/components/Score/index.tsx
@@ -1,3 +1,4 @@
+import { useMemo } from "react"
 import { QuizAnswers, Quiz } from "../../types/quiz"
 
 
@@ -6,9 +7,22 @@ interface ScorePageProps {
     answers: QuizAnswers
 }
 const ScorePageComponent = ({ quiz, answers }: ScorePageProps) => {
+    const score = useMemo(() => {
+        let score = 0
+        const pointsPerQuestion = 100 / quiz.questions.length
+        const questionsMap: { [questionId: number]: number } = quiz.questions.reduce((agg, question,) => ({ ...agg, [question.id]: question.answerId }), {})
+        answers.answers.forEach((answer) => {
+            if (questionsMap[answer.questionId] === answer.answerId) {
+                score += pointsPerQuestion
+            }
+        }
+        )
+        return score
+    }, [quiz, answers])
+
     return (
         <div>
-            hello
+            your score is {score}
         </div>
     )
 }
